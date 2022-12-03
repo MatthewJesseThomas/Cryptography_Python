5c282dbacdb5736292e1f4daeeedcfa50bebd8e5
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-12-03 11:05:15 +0200

    'main'

--- python/Cryptography.py
+++ python/Cryptography.py
@@ -9,3 +9,16 @@ with open('sample.csv','rb') as orig_file:
     original = orig_file.read()
 # Encryption
 encrypted = f.encrypt(original)
+# write encrypted content to a file
+with open ('Sample_en.csv', 'wb') as encrypted_file:
+    encrypted_file.write(encrypted)
+    
+# Load Content of file into memory
+f = Fernet(key)
+with open('Sample_en.csv','rb') as encrypted_file:
+    encrypted = encrypted_file.read()
+
+# write decrypted content to a file
+decrypted = f.decrypt(encrypted)
+with open('Sample_de.csv', 'wb') as decrypted_file:
+    decrypted_file.write(decrypted)
